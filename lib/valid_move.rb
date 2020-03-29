7cfd1c7eb376447a84a274eacb92122bcbdf2d3f
Joshua Kim <jykim92@gmail.com>
2020-03-29 22:15:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
@@ -10,5 +11,19 @@ def position_taken?(board, index)
     return false
   else
     return true
+=======
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between(0, 8)
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  spot = nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    spot = false
+  else
+    spot = true
+>>>>>>> a840112a802ed2b4db0ce431e8ca6129fa036b42
   end
 end
